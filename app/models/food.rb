4861408488d9d7bbf611c764ecd986fb6172cbb8
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-03-06 10:38:33 +0900

    relation models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ApplicationRecord
+    belongs_to :user, :optional => true
     mount_uploader :picture, PictureUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :foods
+  has_many :foodtypes
+  has_many :placetypes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

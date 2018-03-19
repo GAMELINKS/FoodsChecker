9ac3cc3009ee0c1c0b4ef2928dfca8cee9a5d415
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-03-19 10:39:59 +0900

    fix

--- app/controllers/counttypes_controller.rb
+++ app/controllers/counttypes_controller.rb
@@ -27,7 +27,7 @@ class CounttypesController < ApplicationController
   # POST /counttypes.json
   def create
     @counttype = Counttype.new(counttype_params)
-    @counttype.user = current_user
+    @counttype.user_id = current_user
     respond_to do |format|
       if @counttype.save
         format.html { redirect_to @counttype, notice: 'Counttype was successfully created.' }
